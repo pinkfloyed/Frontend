719f74bac2dbec15a91d33b923186a969c722d4f
pinki99 <bootstraphtmljs@gmail.com>
2023-10-09 00:12:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,173 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect, useState } from "react";
 
-function App() {
+function App()
+{
+  const [studentid, setId] = useState('');
+  const [studentname, setName] = useState("");
+  const [studentaddress, setAddress] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [students, setUsers] = useState([]);
+
+useEffect(() => {
+  (async () => await Load())();
+  }, []);
+
+
+  async function  Load()
+  {
+    const result = await axios.get("http://localhost:8080/getAllStudent");
+     setUsers(result.data);
+     console.log(result.data);
+  }
+ 
+     async function save(event)
+    {
+        event.preventDefault();
+    try
+        {
+         await axios.post("http://localhost:8080/saveStudent",
+        {
+          studentname: studentname,
+          studentaddress: studentaddress,
+          mobile: mobile
+        });
+          alert("Student Registation Successfully");
+          setId("");
+          setName("");
+          setAddress("");
+          setMobile("");
+          Load();
+        }
+    catch(err)
+        {
+          alert("User Registation Failed");
+        }
+   }
+
+   async function editStudent(students)
+   {
+    setName(students.studentname);
+    setAddress(students.studentaddress);
+    setMobile(students.mobile); 
+    setId(students.studentid);
+   }
+
+   async function DeleteStudent(studentid)
+   {
+        await axios.delete("http://localhost:8080/updateData" + studentid); 
+        alert("Student deleted Successfully");
+        Load();
+   }
+
+   async function update(event)
+   {
+    event.preventDefault();
+
+   try
+       {
+        await axios.put("http://localhost:8080/putData",
+       {
+        studentid: studentid,
+        studentname: studentname,
+        studentaddress: studentaddress,
+        mobile: mobile
+       
+       });
+         alert("Registation Updateddddd");
+         setId("");
+         setName("");
+         setAddress("");
+         setMobile("");
+         Load();
+       }
+   catch(err)
+       {
+         alert("User Registation Failed");
+       }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+       <h1>Students Details</h1>
+       <div class="container mt-4" >
+          <form>
+              <div class="form-group">
+               <input  type="text" class="form-control" id="studentid" hidden
+               value={studentid}
+               onChange={(event) =>
+                {
+                  setId(event.target.value);      
+                }} 
+               />
+                <label>Student Name</label>
+                <input  type="text" class="form-control" id="studentname"
+                value={studentname}
+                onChange={(event) =>
+                  {
+                    setName(event.target.value);      
+                  }}
+                />
+              </div>
+              <div class="form-group">
+                <label>Student Address</label>
+                <input  type="text" class="form-control" id="studentaddress" 
+                 value={studentaddress}
+                  onChange={(event) =>
+                    {
+                      setAddress(event.target.value);      
+                    }}
+                />
+              </div>
 
+              <div class="form-group">
+                <label>Mobile</label>
+                <input type="text" class="form-control" id="mobile" 
+                  value={mobile}
+                onChange={(event) =>
+                  {
+                    setMobile(event.target.value);      
+                  }}
+                />
+              </div>
+              <div>
+              <button   class="btn btn-primary mt-4"  onClick={save}>Register</button>
+              <button   class="btn btn-warning mt-4"  onClick={update}>Update</button>
+              </div>   
+            </form>
+          </div>
+
+<table class="table table-dark" align="center">
+  <thead>
+    <tr>
+      <th scope="col">Student Id</th>
+      <th scope="col">Student Name</th>
+      <th scope="col">Student Address</th>
+      <th scope="col">Student Mobile</th>
+      
+      <th scope="col">Option</th>
+    </tr>
+  </thead>
+       {students.map(function fn(student)
+       {
+            return(
+            <tbody>
+                <tr>
+                <th scope="row">{student.studentid} </th>
+                <td>{student.studentname}</td>
+                <td>{student.studentaddress}</td>
+                <td>{student.mobile}</td>        
+                <td>
+                    <button type="button" class="btn btn-warning"  onClick={() => editStudent(student)} >Edit</button>  
+                    <button type="button" class="btn btn-danger" onClick={() => DeleteStudent(student.studentid)}>Delete</button>
+                </td>
+                </tr>
+            </tbody>
+            );
+            })}
+            </table>
+       </div>
+            );
+        }
+ 
 export default App;
